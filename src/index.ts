04b54f11c445f556656908c0057498330accbd23
Tyler Williams <sun1231010@gmail.com>
2024-10-17 11:30:56 +0700

    Added http server

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,20 @@
-import express, { Request, Response } from "express";
+import express, { Express, Request, Response } from "express"
+import dotenv from "dotenv"
+import http from "http"
+import cors from "cors"
 
-const app = express();
-const port = process.env.PORT ?? 8080;
+import startServer from "./server"
+
+const app: Express = express()
+const server: http.Server = http.createServer(app)
+
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello, World!");
-});
+  res.send("Hello, World!")
+})
 
-app.listen(port, () => {
-  console.log(`Currently listening on port ${port}`);
-});
+// Enable CORS middleware
+app.use(cors())
+app.options("*", cors())
+
+startServer(server)
