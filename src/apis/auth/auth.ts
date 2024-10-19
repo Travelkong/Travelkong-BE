f6e985848e15db205905eb1bc0d92fbb71ad4440
Tyler <sun1231010@gmail.com>
2024-10-20 00:07:03 +0700

    Improved registering

--- src/apis/auth/auth.ts
+++ src/apis/auth/auth.ts
@@ -1,5 +1,4 @@
-// The ~/ import doesn't work for some reason
-import { generateAuthenticationToken } from '~/middlewares'
+import { generateAuthenticationToken } from "~/middlewares"
 import jwt from "jsonwebtoken"
 import express, { NextFunction, Request, Response } from "express"
 const router = express.Router()
@@ -16,21 +15,21 @@ router.post("/register", async (req: Request, res: Response): Promise<void> => {
     username: req.body.username,
     email: req.body.email,
   })
-  res.status(200).json(token)
+  res.status(200).json({ token })
 })
 
 function authenticateToken(req: IUser, res: Response, next: NextFunction) {
   const authHeader = req.headers["authorization"]
   const authToken: string | undefined = authHeader?.split(" ")[1]
 
-  if (!authToken) return res.status(401).send("Invalid token")
+  if (!authToken) return res.status(401).json({ error: "Missing token!" })
   jwt.verify(
     authToken,
     process.env.JWT_TOKEN as string,
     (err: any, username: any) => {
       if (err) {
         console.log(err)
-        res.status(403)
+        res.status(403).json({ error: "Invalid or expired token!" })
       }
 
       req.username = username
