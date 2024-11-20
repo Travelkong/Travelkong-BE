46e3a6f3b34bb2771fef644b97d507ac76b3223e
Kira Toryu <127835141+thuan122@users.noreply.github.com>
2024-11-20 10:57:29 +0700

    basic routing and DTO for Post

---
+++ src/apis/post/post.route.ts
@@ -0,0 +1,10 @@
+import express from "express"
+
+
+const router = express.Router()
+
+router.post("/createPost",)
+router.put("/editPost", )
+router.delete("/deletePost", )
+
+export default router
--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,10 @@
 import { Express } from "express"
 import AuthRoute from "./apis/auth"
+import PostRoute from "./apis/post"
 
 const initRoutes = (app: Express): any => {
     app.use("/apis/auth", AuthRoute)
+    app.use("/apis/post", PostRoute)
 
     // Fall back route
     return app.get(/.*/, (req, res) => {
