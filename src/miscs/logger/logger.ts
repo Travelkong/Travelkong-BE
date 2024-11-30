4aa6d0f3e879af116e75404b016b1789ce6920ba
Tyler Williams <sun1231010@gmail.com>
2024-11-30 15:38:39 +0700

    Feat: Refactored logger

--- src/miscs/logger/logger.ts
+++ src/miscs/logger/logger.ts
@@ -1,14 +1,14 @@
 import logger from "~/configs/winston.config"
 
 export class Logger {
-  private readonly noStackTrace: string = "No stack trace available!"
-  private readonly noMessage: string = "No message available!"
+  private readonly _noStackTrace: string = "No stack trace available!"
+  private readonly _noMessage: string = "No message available!"
 
   public error = (err: Error) => {
     logger.log({
       level: "error",
-      message: err.message ?? this.noMessage,
-      stack: err.stack ?? this.noStackTrace,
+      message: err.message ?? this._noMessage,
+      stack: err.stack ?? this._noStackTrace,
     })
   }
 }
