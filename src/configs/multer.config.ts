fb794af405a9f2ef7cc785bbec529044ecaaf541
Tyler <sun1231010@gmail.com>
2024-12-11 21:22:35 +0700

    Feat: Implementing image uploading feature

---
+++ src/configs/multer.config.ts
@@ -0,0 +1,19 @@
+import dotenv  from 'dotenv';
+import multer from "multer";
+import path from 'path';
+
+dotenv.config()
+
+const uploadsPath: string = process.env.UPLOADS_PATH ?? "~/uploads"
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, uploadsPath)
+    },
+    filename: (req, file, cb) => {
+        const timestamp: number = Date.now()
+        cb(null, `${timestamp}_` + path.extname(file.originalname))
+    }
+})
+
+export const upload = multer({ storage })
