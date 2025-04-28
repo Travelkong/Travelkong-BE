d0f5059bf243ea05960dbe205064855e0b237c61
Tyler <sun1231010@gmail.com>
2025-04-29 00:51:45 +0700

    Refactor: Optimized the runtime to O(n)

--- src/miscs/utils/sql/insertBuilder.ts
+++ src/miscs/utils/sql/insertBuilder.ts
@@ -1,34 +1,34 @@
 import QueryBuilder from "./queryBuilder"
 
 export default class InsertBuilder extends QueryBuilder<InsertBuilder> {
-  private readonly _params: unknown[] = []
-  private _paramCounter = 1
+  private readonly _params: unknown[] = [] // An array of user-inputted data
 
-  // TODO: optimize this so that it wouldn't be O(n^2).
-  private _column<T>(keys: string[], values: T[], params: T[]): string[] {
-    const valueArr = []
-    for (let i = 0; i < keys.length; i++) {
-      valueArr.push(`${keys[i]}`)
-      params.push(values[i])
-      this._paramCounter++
-    }
+  private _columns<T>(
+    keys: string[],
+    values: T[],
+    params: T[],
+  ): [string[], string[]] {
+    const columns: string[] = []
+    const placeholders: string[] = []
 
-    return valueArr
+    keys.forEach((key, index) => {
+      columns.push(`${key}`)
+      params.push(values[index])
+      placeholders.push(`$${index + 1}`)
+    })
+
+    return [columns, placeholders]
   }
 
-  private _setPlaceholders(keys: string[]): string[] {
-    const placeholders = []
-    for (let i = 1; i <= keys.length; i++) {
-      placeholders.push(`$${i}`)
-    }
+  build(): { query: string; values: unknown[] } {
+    const [columns, placeholders] = this._columns(
+      this._keys,
+      this._values,
+      this._params,
+    )
 
-    return placeholders
-  }
-
-  build(): {query: string; values: unknown[]} {
-    let query = `INSERT INTO ${this._table} `
-    query += `(${this._column(this._keys, this._values, this._params).join(", ")}) VALUES `
-    query += `(${this._setPlaceholders(this._keys).join(", ")})`
+    // It would look like this: "INSERT INTO table (column1, column2) VALUES ($1, $2)"
+    const query = `INSERT INTO ${this._table} (${columns}) VALUES (${placeholders})`
 
     return { query: query, values: this._values }
   }
