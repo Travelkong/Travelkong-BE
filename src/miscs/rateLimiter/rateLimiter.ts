fe2aea787887f90a4f67f3c644b4a4f06f469e35
Tyler <sun1231010@gmail.com>
2025-06-26 08:32:27 +0700

    Refactor (rate limit): Changed the limit to 100 API calls per 15 mins

--- src/miscs/rateLimiter/rateLimiter.ts
+++ src/miscs/rateLimiter/rateLimiter.ts
@@ -2,7 +2,7 @@ import { rateLimit } from "express-rate-limit"
 
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000,
-    limit: 200,
+    limit: 100,
     message: "Too many request from this IP.",
     standardHeaders: "draft-8",
     legacyHeaders: false
