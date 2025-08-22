f9fad6e92884711e91820e9a940281ce1bcd1c5d
Tyler <sun1231010@gmail.com>
2025-08-22 07:40:53 +0700

    feat!: adding firebase and mongodb

--- src/configs/env.config.ts
+++ src/configs/env.config.ts
@@ -34,6 +34,20 @@ const EnvConfig = {
     clientSecret: process.env.FACEBOOK_APP_SECRET,
   },
 
+  firebase: {
+    type: "service_account",
+    universeDomain: "googleapis.com",
+    tokenUri: "https://oauth2.googleapis.com/token",
+    authUri: "https://accounts.google.com/o/oauth2/auth",
+    authProviderX509CertUrl: "https://www.googleapis.com/oauth2/v1/certs",
+
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    privateKeyId: process.env.FIREBASE_PRIVATE_KEY_ID,
+    privateKey: process.env.FIREBASe_PRIVATE_KEY,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    clientId: process.env.FIREBASE_CLIENT_ID,
+    clientX509CertUrl: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+  },
   opensearchUrl: process.env.OPENSEARCH_URL,
 }
 
